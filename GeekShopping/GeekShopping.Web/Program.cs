3d704c8b9e7aa6a0c1feeaebed6bb037cee6efa3
Douglas <douglasdcasagrande@hotmail.com>
2026-08-23 22:10:36 -0300

    Código da seção 16

--- GeekShopping/GeekShopping.Web/Program.cs
+++ GeekShopping/GeekShopping.Web/Program.cs
@@ -35,6 +35,10 @@ builder.Services.AddHttpClient<ICartService, CartService>(c =>
         c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"])
     );
 
+//builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
+//        c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"])
+//    );
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
